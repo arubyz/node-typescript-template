f13e6a1ad2cc8c6a7b8519f0aee1f9ca2c29f87b
Jay Zybura <jay.zybura@strivr.com>
2023-04-13 18:50:09 -0500

    cleanup format script

--- scripts/format.ts
+++ scripts/format.ts
@@ -3,13 +3,14 @@ import { $, script } from './lib/utils.js';
 const files = ['src/**/*.ts', 'scripts/**/*.ts'];
 
 export default script(async ([cmd]) => {
-    if (cmd !== 'check' && cmd !== 'fix') {
-        throw new Error('Must specify either "check" or "write"');
-    }
-
-    if (cmd === 'check') {
-        await $`yarn --silent run prettier --check ${files}`;
-    } else {
-        await $`yarn --silent run prettier --write ${files}`;
+    switch (cmd) {
+        case 'check':
+            await $`yarn --silent run prettier --check ${files}`;
+            break;
+        case 'fix':
+            await $`yarn --silent run prettier --write ${files}`;
+            break;
+        default:
+            throw new Error('Must specify either "check" or "fix"');
     }
 });
